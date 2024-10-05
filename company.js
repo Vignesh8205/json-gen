638e84e2a4bc1951c05349eb492a0eb15a64a6af
Vignesh8205 <115607497+Vignesh8205@users.noreply.github.com>
2024-10-05 22:59:20 +0530

    first commit

---
+++ company.js
@@ -0,0 +1,154 @@
+let company = [
+    {
+        id:"1",
+        companyName:"panamax infotech",
+        url:"https://www.panamaxil.com/careers"
+    },
+    {
+        id:"2",
+        companyName:"Infrabeat",
+        url:"https://infrabeat.com/pages/career/"
+    },
+    {
+        id:"3",
+        companyName:"ITAC",
+        url:"https://www.itacsoftware.com/en/career"
+    },
+    {
+        id:"4",
+        companyName:"COPIA",
+        url:"https://www.copia.io/careers"
+    },
+    {
+        id:"5",
+        companyName:"Mphasis",
+        url:"https://careers.mphasis.com/home.html"
+    },
+    {
+        id:"6",
+        companyName:"Visionet",
+        url:"https://www.visionet.com/job"
+    },
+    {
+        id:"7",
+        companyName:"persistent System",
+        url:"https://careers.persistent.com/#!/"
+    },
+    {
+        id:"8",
+        companyName:"Infinite",
+        url:"https://www.infinite.com/careers/"
+    },
+    {
+        id:"9",
+        companyName:"Valuelabs",
+        url:"https://careers.valuelabs.com/"
+    },
+    {
+        id:"10",
+        companyName:"Ust Global",
+        url:"https://www.ust.com/en/careers"
+    },
+    {
+        id:"11",
+        companyName:"QISoft Technologies PVT LTD",
+        url:"https://qsoft.pk/careers.htm"
+    },
+    {
+        id:"12",
+        companyName:"Yethi consulting Pvt Ltd",
+        url:"https://yethi.in/careers"
+    },
+    {
+        id:"13",
+        companyName:"MLE system",
+        url:"https://www.mlesystems.com/careers/"
+    },
+    {
+        id:"14",
+        companyName:"Tekkol global company",
+        url:"https://www.tekksolglobal.com/careers"
+    },
+    {
+        id:"15",
+        companyName:"GSR Business Services",
+        url:"https://gsr-inc.com/category/careers/india/"
+    },
+    {
+        id:"16",
+        companyName:"Worldline global",
+        url:"https://jobs.worldline.com/content/Tech-Careers/?locale=en_US"
+    },
+    {
+        id:"17",
+        companyName:"LTI",
+        url:"https://www.ltimindtree.com/india-careers/"
+    },
+    {
+        id:"18",
+        companyName:"Capgemini",
+        url:"https://www.capgemini.com/in-en/careers/"
+    },
+    {
+        id:"19",
+        companyName:"Sdet Wissen",
+        url:"https://www.wissen.com/career/opportunities-wissen-infotech"
+    },
+    {
+        id:"20",
+        companyName:"NTT",
+        url:"https://careers.services.global.ntt/global/en"
+    },
+    {
+        id:"21",
+        companyName:"Apex IT Services",
+        url:"https://apex-it-solutions.com/careers.html"
+    },
+    {
+        id:"22",
+        companyName:"Coforge",
+        url:"https://careers.coforge.com/#!/"
+    },
+    {
+        id:"23",
+        companyName:"Crown solutions",
+        url:"https://www.crownsolution.com/careers/"
+    },
+    {
+        id:"24",
+        companyName:"Ernst and young",
+        url:"https://www.ey.com/en_in/careers/job-search"
+    },
+    {
+        id:"25",
+        companyName:"Erobern Solution Pvt ltd",
+        url:"https://erobern.in/about/#"
+    },
+    {
+        id:"26",
+        companyName:"Quality kisok",
+        url:"https://qualitykiosk.com/careers/"
+    },
+    {
+        id:"27",
+        companyName:"CGI",
+        url:"https://www.cgi.com/en/careers"
+    },
+    {
+        id:"28",
+        companyName:"Huron",
+        url:"https://www.huronconsultinggroup.com/company/careers"
+    },
+    {
+        id:"30",
+        companyName:"alchemy",
+        url:"https://www.alchemy.com/careers"
+    },
+    {
+        id:"31",
+        companyName:"Intellect",
+        url:"https://www.alchemy.com/careers"
+    },
+    
+    
+]
